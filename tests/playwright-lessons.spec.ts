a177443387c7268a61e38541f3433dc954cdbcd6
Guy Levin <guy.f.levin@gmail.com>
2026-03-26 17:01:37 +0200

    debugged and fixed test

---
+++ tests/playwright-lessons.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('fill and submit form', async ({ page }) => {
+  await page.goto('https://practicesoftwaretesting.com/');
+  await page.getByText('Sign In').click();
+  await page.fill('#email', 'customer@practicesoftwaretesting.com');
+  await page.fill('#password', 'welcome01');
+  await page.click('.btnSubmit:has-text("Login")');
+  await page.waitForURL('**/account');
+  expect(await page.url()).toContain('/account');
+});
+
+test('title', async ({ page }) => {
+  await page.goto('https://practicesoftwaretesting.com/');
+  const title = await page.locator('title').textContent();
+  expect(title).toBe('Practice Software Testing - Toolshop - v5.0');
+});
+
+test('get all cards', async ({ page }) => {
+  await page.goto('https://practicesoftwaretesting.com/');
+  const cards = await page.locator('.card').all();
+  expect(cards.length).toBeGreaterThanOrEqual(8);
+  expect(cards.length).toBeLessThanOrEqual(9); // Depending on the version of the page, there might be 8 or 9 cards
+});
+
+// don't run this test with webkit
+test('get card title', async ({ page }) => {
+  await page.goto('https://practicesoftwaretesting.com/');
+  const cardTitle = await page.locator('.card .card-title').textContent();
+  expect(cardTitle).toBe('Selenium WebDriver');
+});
